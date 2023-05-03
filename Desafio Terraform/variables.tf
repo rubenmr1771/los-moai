fb78bbe30e10ffa5880fb431d9a0b5a9e652f6d9
Ruben Juan Garcia Suarez <ruben-juan.garcia-suarez.external@capgemini.com>
2023-05-03 10:29:49 +0200

    Añadidas variables de usuario para el provider

--- Desafio Terraform/variables.tf
+++ Desafio Terraform/variables.tf
@@ -4,15 +4,31 @@ variable "region" {
 }
 
 variable "idmaquina" {
-    default = "ID" //ID Máquina Virtual
-    description = "ID de la máquina de Azure"
+  default = "ID" //ID Máquina Virtual
+  description = "ID de la máquina de Azure"
 }
 
 variable "tipomaquina" {
-    default = "Tipo" //Tipo de máquina
-    description = "Tipo de máquina"
+  default = "Tipo" //Tipo de máquina
+  description = "Tipo de máquina"
+}
+
+variable "subscripcion" {
+  default = "Moai" //ID de subscripción de Azure
+  description = "ID de la suscripción"
 }
 
 variable "usuario" {
-  
+  default = "Moaicapgemini" //Id de Usuario de Azure
+  description = "ID del usuario"
+}
+
+variable "contrasena" {
+  default = "losmoai" //Contraseña de usuario de Azure
+  description = "Contraseña de acceso a Azure"
+}
+
+variable "invitado" {
+  default = "samu" //Id de acceso de invitado a la máquina
+  description = "ID de acceso como invitado o tenant a la máquina"
 }
