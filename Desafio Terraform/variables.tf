7c3ee2eac4c63d39eb6ad3c0828cfce3e48f465e
Ruben Juan Garcia Suarez <ruben-juan.garcia-suarez.external@capgemini.com>
2023-05-03 10:50:45 +0200

    Creado tfvars

--- Desafio Terraform/variables.tf
+++ Desafio Terraform/variables.tf
@@ -1,3 +1,5 @@
+//Fichero de variables del módulo
+
 variable "region" {
   default     = "eu-west" //Servidor de Azure
   description = "Dirección del servidor Azure donde etá ubicada la máquina"
@@ -11,24 +13,4 @@ variable "idmaquina" {
 variable "tipomaquina" {
   default = "Tipo" //Tipo de máquina
   description = "Tipo de máquina"
-}
-
-variable "subscripcion" {
-  default = "Moai" //ID de subscripción de Azure
-  description = "ID de la suscripción"
-}
-
-variable "usuario" {
-  default = "Moaicapgemini" //Id de Usuario de Azure
-  description = "ID del usuario"
-}
-
-variable "contrasena" {
-  default = "losmoai" //Contraseña de usuario de Azure
-  description = "Contraseña de acceso a Azure"
-}
-
-variable "invitado" {
-  default = "samu" //Id de acceso de invitado a la máquina
-  description = "ID de acceso como invitado o tenant a la máquina"
 }
---
+++ Desafio Terraform/vars/terraform-dev.tfvars
@@ -0,0 +1,25 @@
+//Fichero de variables globales
+
+instance_type = "A-Series"
+tag = "Instancia para máquina azure"
+location = "eu-west"
+
+variable "subscripcion" {
+  default = "Moai" //ID de subscripción de Azure
+  description = "ID de la suscripción"
+}
+
+variable "usuario" {
+  default = "Moaicapgemini" //Id de Usuario de Azure
+  description = "ID del usuario"
+}
+
+variable "contrasena" {
+  default = "losmoai" //Contraseña de usuario de Azure
+  description = "Contraseña de acceso a Azure"
+}
+
+variable "invitado" {
+  default = "samu" //Id de acceso de invitado a la máquina
+  description = "ID de acceso como invitado o tenant a la máquina"
+}
