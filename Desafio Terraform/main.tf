8d6da4497a398dfa716baf20fe7e93caf0356adb
Ruben Juan Garcia Suarez <ruben-juan.garcia-suarez.external@capgemini.com>
2023-05-03 10:18:51 +0200

    Rama Dev Push

--- Desafio Terraform/main.tf
+++ Desafio Terraform/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "moai" {
     location = var.region //Conectamos al servidor de azure mediante la variable region especificada en el fichero variables
-    name = "Máquina del grupo Los Moai"
+    name = "Máquina del grupo Los Moai para el desafio Azure"
 }
 
